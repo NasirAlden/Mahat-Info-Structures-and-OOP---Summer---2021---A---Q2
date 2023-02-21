7f5e104c1571b0ee08f16a744966b3280b013c52
NasirAlden <109313775+NasirAlden@users.noreply.github.com>
2023-02-21 13:53:40 +0200

    Update Program.cs

--- Mahat Info Structures and OOP - Summer - 2021 - A - Q2/Program.cs
+++ Mahat Info Structures and OOP - Summer - 2021 - A - Q2/Program.cs
@@ -8,31 +8,36 @@ namespace Mahat_Info_Structures_and_OOP___Summer___2021___A___Q2
 {
     internal class Program
     {
-        public static List<int> FilterDuplicates(List<int> numbers)
+         public static List<int> FilterDuplicates(List<int> numbers)
         {
             List<int> filteredList = new List<int>();
+            int count = 0;
             foreach (int n in numbers)
             {
-                if (numbers.IndexOf(n) != numbers.LastIndexOf(n) && !filteredList.Contains(n))
-                {
-                    filteredList.Add(n);
+                for (int i = numbers.IndexOf(n); i < numbers.Count() - 1; i++) {
+                    if (n == numbers[i])
+                        count++;
                 }
+                if (count >=3 && !filteredList.Contains(n))
+                    filteredList.Add(n);
+                count=0;
             }
             return filteredList;
         }
-
         static void Main(string[] args)
         {
+
             List<int> queue = new List<int>();
+            List<int> filteredQueue = new List<int>();
+
             int[] elements = { 2, 5, 5, 7, 2, 4, 1, 3, 2, 5, 5, 1 };
             foreach (int element in elements)
             {
                 queue.Add(element);
             }
 
-            
-
-
+            filteredQueue= FilterDuplicates(queue);
+            Console.WriteLine(String.Join(",", filteredQueue)); 
         }
     }
 }
